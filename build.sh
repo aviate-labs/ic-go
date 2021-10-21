81901e58cac305ddf866eec40796b92a787ed057
Quint Daenen <me@di-wu.be>
2021-10-21 15:03:11 +0200

    Fix build.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-GOOS=js GOARCH=wasm go build -ldflags="-s -w" -o src/go_assets/assets/main.wasm
-cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" src/go_assets/src/wasm_exec.js
+GOOS=js GOARCH=wasm go build -ldflags="-s -w" -o src/example/assets/main.wasm
+cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" src/example/src/wasm_exec.js
